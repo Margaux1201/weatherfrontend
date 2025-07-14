3f63498ad4c2064160310be320d7707398bfc946
Margaux <margaux.courageux@gmail.com>
2025-07-14 11:08:07 +0200

    clean du fichier js login + ajout des alertes au clic des formulaires + réinitialisation des champs au clic

--- login.js
+++ login.js
@@ -1,16 +1,19 @@
-// Insert your code here
+// Retour à la page d'accueil (index.html)
 document.querySelector("#homeButton").addEventListener("click", function () {
   window.location.assign("index.html");
 });
 
+// Ajout d'un utilisateur dans la base de données
 document.querySelector("#register").addEventListener("click", function () {
+  // définition de la variable user avec les valeurs des champs du formulaire d'inscription
   const user = {
     name: document.querySelector("#registerName").value,
     email: document.querySelector("#registerEmail").value,
     password: document.querySelector("#registerPassword").value,
   };
 
-  fetch("https://weatherbackend-peach.vercel.app/users/signup", {
+  // Envoi des données de user vers le backend pour l'inscription
+  fetch("http://localhost:3000/users/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
@@ -18,18 +21,35 @@ document.querySelector("#register").addEventListener("click", function () {
     .then((response) => response.json())
     .then((data) => {
       if (data.result) {
+        // Réinitialisation des champs du formulaire d'inscription
+        ["#registerName", "#registerEmail", "#registerPassword"].forEach(
+          (selector) => {
+            document.querySelector(selector).value = "";
+          }
+        );
+        alert("Inscription réussie");
         window.location.assign("index.html");
+      } else {
+        ["#registerName", "#registerEmail", "#registerPassword"].forEach(
+          (selector) => {
+            document.querySelector(selector).value = "";
+          }
+        );
+        alert(data.error);
       }
     });
 });
 
+// Connexion d'un utilisateur existant dans la base de données
 document.querySelector("#connection").addEventListener("click", function () {
+  // définition de la variable user avec les valeurs des champs du formulaire de connexion
   const user = {
     email: document.querySelector("#connectionEmail").value,
     password: document.querySelector("#connectionPassword").value,
   };
 
-  fetch("https://weatherbackend-peach.vercel.app/users/signin", {
+  // Envoi des données de user vers le backend pour la connexion
+  fetch("http://localhost:3000/users/signin", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
@@ -37,7 +57,17 @@ document.querySelector("#connection").addEventListener("click", function () {
     .then((response) => response.json())
     .then((data) => {
       if (data.result) {
+        // Réinitialisation des champs du formulaire de connexion
+        ["#registerEmail", "#registerPassword"].forEach((selector) => {
+          document.querySelector(selector).value = "";
+        });
+        alert("Connexion réussie");
         window.location.assign("index.html");
+      } else {
+        ["#connectionEmail", "#connectionPassword"].forEach((selector) => {
+          document.querySelector(selector).value = "";
+        });
+        alert(data.error);
       }
     });
 });
